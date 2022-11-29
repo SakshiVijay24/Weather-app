7d91b297d8770e297cf8c1e3b29bc125436dd2cf
SakshiVijay24 <sakshivijay24@gmail.com>
2022-11-29 10:55:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './index.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home"
+import Next from "./Components/Next"
+function App() {
+  return (
+
+
+<Router>
+<Switch>
+  <Route exact path="/" component={Home} />
+  <Route exact path="/next" component={Next} />
+  
+</Switch>
+</Router>
+  );
+}
+
+export default App;
